e2a5a22817dc5358c9215ddff01340a34101f4b5
Sam Wen <samw@ritani.com>
2022-07-02 21:19:39 -0400

    update

--- src/app/get-config-entity.js
+++ src/app/get-config-entity.js
@@ -1,36 +1,24 @@
 'use strict';
 
 module.exports = (input) => {
-    let {data, attributes, ...rest} = input;
+    let {attributes, ...rest} = input;
     if (attributes && !Array.isArray(attributes)) {
         const items = [];
         for (const [name, value] of Object.entries(attributes)) {
-            if (!name) continue;
+            if (!name || value === null) continue;
             items.push(get_component_item(name, value));
         }
         if (items.length > 0) attributes = items;
         else attributes = null;
     }
     if (attributes) rest.attributes = attributes;
-    if (data && !Array.isArray(data)) {
-        const items = [];
-        for (const [name, value] of Object.entries(data)) {
-            if (!name) continue;
-            items.push(get_component_item(name, value));
-        }
-        if (items.length > 0) data = items;
-        else data = null;
-    }
-    if (data) rest.data = data;
     return rest;
 }
 
 function get_component_item(name, value) {
     const type = typeof value;
-    let __component = 'pfapi-types.text';
-    if (value === null) {
-        return {__component, name, value};
-    } else if (type === 'number') {
+    let __component = 'pfapi-types.text';  
+    if (type === 'number') {
         if (Number.isInteger(value)) __component = 'pfapi-types.integer';
         else __component = 'pfapi-types.decimal';
     } else if (type === 'boolean') {
